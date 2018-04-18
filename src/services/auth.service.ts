fc116c660a7aa66e1671268338b08ec9adc418e4
Jessica Santos <jel_008@hotmail.com>
2018-04-18 15:27:15 -0300

    Começando a implementar a autenticação

---
+++ src/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+import { CredeniciaisDTO } from '../models/credenciais.dto';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from "../config/api.config";
+
+@Injectable()
+export class AuthService {
+
+
+  constructor(public http: HttpClient){
+
+  }
+
+  authenticate(creds : CredeniciaisDTO){
+
+   return this.http.post(
+      `${API_CONFIG.baseUrl}/login`,
+       creds,
+      {
+        observe : 'response',
+        responseType : 'text'
+      })
+  }
+}
