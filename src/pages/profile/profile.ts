bbffa9bef322605294b07e99753156ff29107d0c
Jessica Santos <jel_008@hotmail.com>
2018-04-18 23:05:35 -0300

    Mostrando dados e imagem do cliente para página de profile [continuacao]

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { StorageService } from '../../services/storage.service';
 import { ClienteDTO } from '../../models/cliente.dto';
 import { ClienteService } from '../../services/domain/cliente.service';
+import { API_CONFIG } from '../../config/api.config';
 
 @IonicPage()
 @Component({
@@ -26,10 +27,17 @@ export class ProfilePage {
       this.clienteService.findByEmail(localUser.email)
       .subscribe(response => {
         this.cliente = response;
-
+        this.getImageExists();
       },
       error => {});
     }
   }
 
+  getImageExists() {
+    this.clienteService.getImageFromBucket(this.cliente.id)
+      .subscribe(response =>{
+        this.cliente.imageUrl = `${API_CONFIG.bucketBaseUrl}/cp${this.cliente.id}.jpg`
+      })
+
+  }
 }
--- src/services/domain/cliente.service.ts
+++ src/services/domain/cliente.service.ts
@@ -25,4 +25,10 @@ export class ClienteService {
       {'headers': authHeader});
   }
 
+  getImageFromBucket(id : string) : Observable<any> {
+    let url = `${API_CONFIG.bucketBaseUrl}/cp${id}.jpg`
+    return this.http.get(url, {responseType : 'blob'});
+  }
+
+
 }
