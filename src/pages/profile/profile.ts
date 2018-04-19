310ad268f483ff8ea6ddc3cc75a23560c6f24f7c
Jessica Santos <jel_008@hotmail.com>
2018-04-18 22:32:21 -0300

    Mostrando dados e imagem do cliente para página de profile

---
+++ src/pages/profile/profile.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { StorageService } from '../../services/storage.service';
+import { ClienteDTO } from '../../models/cliente.dto';
+import { ClienteService } from '../../services/domain/cliente.service';
+
+@IonicPage()
+@Component({
+  selector: 'page-profile',
+  templateUrl: 'profile.html',
+})
+export class ProfilePage {
+
+  cliente : ClienteDTO;
+
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    public storage: StorageService,
+    public clienteService: ClienteService) {
+  }
+
+  ionViewDidLoad() {
+    let localUser = this.storage.getLocalUser();
+    if(localUser && localUser.email) {
+      this.clienteService.findByEmail(localUser.email)
+      .subscribe(response => {
+        this.cliente = response;
+
+      },
+      error => {});
+    }
+  }
+
+}
