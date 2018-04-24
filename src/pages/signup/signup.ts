b8cac7179a7371bebd877e7fccc45c973f8edfd4
Jessica Santos <jel_008@hotmail.com>
2018-04-24 20:26:35 -0300

    Mostrando erros de validação

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -26,19 +26,19 @@ export class SignupPage {
       this.formGroup = this.formBuilder.group({
         nome: ['Joaquim', [Validators.required, Validators.minLength(5), Validators.maxLength(80)]],
         email: ['joaquim@gmail.com', [Validators.required, Validators.email]],
-        tipo : ['1', [Validators.required]],
-        cpfOuCnpj : ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
-        senha : ['12345', [Validators.required, Validators.minLength(5), Validators.maxLength(8)]],
-        logradouro : ['Rua Via', [Validators.required]],
-        numero : ['25', [Validators.required]],
-        complemento : ['Apto 3', []],
-        bairro : ['Copacabana', []],
-        cep : ['10828333', [Validators.required]],
-        telefone1 : ['977261827', [Validators.required]],
-        telefone2 : ['', []],
-        telefone3 : ['', []],
-        estadoId : [null, [Validators.required]],
-        cidadeId : [null, [Validators.required]]
+        tipo: ['1', [Validators.required]],
+        cpfOuCnpj: ['06134596280', [Validators.required, Validators.minLength(11), Validators.maxLength(14)]],
+        senha: ['12345', [Validators.required, Validators.minLength(5), Validators.maxLength(8)]],
+        logradouro: ['Rua Via', [Validators.required]],
+        numero: ['25', [Validators.required]],
+        complemento: ['Apto 3', []],
+        bairro: ['Copacabana', []],
+        cep: ['10828333', [Validators.required]],
+        telefone1: ['977261827', [Validators.required]],
+        telefone2: ['', []],
+        telefone3: ['', []],
+        estadoId: [null, [Validators.required]],
+        cidadeId: [null, [Validators.required]]
       });
   }
 
