31dee5ea98e67f660cc3dfea78cc9756458b6956
Jessica Santos <jel_008@hotmail.com>
2018-05-07 23:51:12 -0300

    Acesso ao Carrinho: Botao Flutuante!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,6 +27,7 @@ export class MyApp {
     this.pages = [
       { title: 'Perfil', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
+      {title: 'Carrinho', component: 'CartPage'},
       {title: 'Sair', component: ''}
     ];
 
